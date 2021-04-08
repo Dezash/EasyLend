2f32d0f158e8354cb1dd081863a6f03fd0d70220
Gabrielius Laurinavičius <gabrielius.laurinavicius@gmail.com>
2021-04-08 22:48:38 +0300

    Add navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navigation from './components/Navigation'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Router>
+        <Switch>
+          <Route path='/' exact render={() => (
+            <h1>Hello</h1>
+          )}></Route>
+          <Route render={() => (
+            <h1>404 not found</h1>
+          )}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
