d46948503ce70d07735fc4826706e2213df179f3
Tomas Butkus <tomasbutkus@outlook.com>
2021-04-09 13:43:39 +0300

    cors, db initialization, GET returns users

---
+++ backend/easylend/easylend/Database/ApplicationContext.cs
@@ -0,0 +1,37 @@
+﻿using easylend.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace easylend.Database
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Application> Applications { get; set; }
+        public DbSet<Document> Documents { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            //
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.HasOne(a => a.Application).WithOne(u => u.User).IsRequired(false);
+            });
+
+            modelBuilder.Entity<Application>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Status).HasConversion(v => v.ToString(), v => (Status)Enum.Parse(typeof(Status), v));
+                entity.HasMany(d => d.Documents).WithOne(a => a.Application);
+            });
+
+        }
+    }
+}
---
+++ backend/easylend/easylend/Database/Entities/Application.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace easylend.Entities
+{
+    public class Application
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public Status Status { get; set; }
+
+        [JsonIgnore]
+        public User User { get; set; }
+        [JsonIgnore]
+        public ICollection<Document> Documents { get; set; }
+
+        public Application()
+        {
+            Documents = new List<Document>();
+        }
+    }
+
+    public enum Status
+    {
+        Approved,
+        Rejected,
+        Pending
+    }
+}
