88e8d758f255d5179c3395885fe88a1414fff8c4
Aistis Olendra <aistis3211@gmail.com>
2021-04-19 02:04:19 +0300

    Implement all backend stuff

---
+++ backend/easylend/easylend/DTO/NewApplicationDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using easylend.Entities;
+
+namespace easylend.DTO
+{
+    public class NewApplicationDTO
+    {
+        public DateTime Date { get; set; }
+        public Status Status { get; set; }
+        public NewUserDTO User { get; set; }
+        public List<NewDocumentDTO> Documents { get; set; }
+    }
+}
--- backend/easylend/easylend/DTO/UserDTO.cs
+++ backend/easylend/easylend/DTO/UserDTO.cs
@@ -1,28 +1,18 @@
 ﻿using System;
-using easylend.Database.Entities;
-using Newtonsoft.Json;
 
-namespace easylend.Entities
+namespace easylend.DTO
 {
-    public class User
+    public class UserDTO
     {
-        [JsonIgnore]
         public int Id { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
         public DateTime Birthdate { get; set; }
         public DateTime DateRegistered { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
         public string PersonalCode { get; set; }
         public string Address { get; set; }
-        public string PhoneNumber { get; set; }
+        public string Phone { get; set; }
         public double MinInterestRate { get; set; }
-        public virtual Application Application { get; set; }
-
-        public User()
-        {
-            DateRegistered = DateTime.Now;
-        }
     }
 }
