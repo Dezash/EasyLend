feda4041e8b7aba1f7108a6acf2d645a36c6d882
Aistis Olendra <aistis3211@gmail.com>
2021-05-17 01:32:40 +0300

    asd

--- backend/easylend/easylend/DTO/NewApplicationDTO.cs
+++ backend/easylend/easylend/DTO/NewApplicationDTO.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using easylend.Entities;
 
 namespace easylend.DTO
 {
     public class NewApplicationDTO
     {
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
         public Status Status { get; set; }
         public NewUserDTO User { get; set; }
